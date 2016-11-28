7aec4a12e1051afabdc6a26e2266ddd07ff2ed7b
Branan Riley <branan@puppet.com>
2016-11-28 14:08:06 -0800

    Allow passing in CMake options from the environment
    Also a quick tweak to ensure that we use the Leatherman that was built
    locally, rather than whatever we find on the system

--- ext/facter/extconf.rb
+++ ext/facter/extconf.rb
@@ -3,6 +3,7 @@ require 'erb'
 
 cmake_binary = find_executable('cmake')
 source_root = File.expand_path("#{__FILE__}/..")
+cmake_opts = ENV['FACTER_CMAKE_OPTS']
 
 make_template = File.open(source_root+"/Makefile.erb").read
 makefile = ERB.new(make_template).result(binding)
